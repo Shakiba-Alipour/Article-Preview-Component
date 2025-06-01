ac81c2a935ff96d3df26f1b89aa921f9913679b2
Shakiba Alipour <alipour.sh79@gmail.com>
2025-06-01 18:50:45 +0330

    desktop active footer is completed

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const shareActivateButton = document.getElementById("inactive-share");
 const shareDeactivateButton = document.getElementById("active-share");
-const activeFooter = document.getElementById("active-footer");
 const inactiveFooter = document.getElementById("inactive-footer");
 
-shareActivateButton.addEventListener("click", () => {
-  activeFooter.style.display = "flex";
-  inactiveFooter.style.display = "none";
-});
+let activeFooter = false;
 
-shareDeactivateButton.addEventListener("click", () => {
-  activeFooter.style.display = "none";
-  inactiveFooter.style.display = "flex";
-});
+if (window.innerWidth <= 700) {
+  activeFooter = document.getElementById("mobile-active-footer");
+
+  shareActivateButton.addEventListener("click", () => {
+    activeFooter.style.display = "flex";
+    inactiveFooter.style.display = "none";
+  });
+
+  shareDeactivateButton.addEventListener("click", () => {
+    activeFooter.style.display = "none";
+    inactiveFooter.style.display = "flex";
+  });
+} else {
+  activeFooter = document.getElementById("desktop-active-footer");
+  let isFooterActive = false; // Moved here and changed to let
+
+  shareActivateButton.addEventListener("click", () => {
+    isFooterActive = !isFooterActive;
+
+    if (isFooterActive) {
+      activeFooter.style.display = "flex";
+    } else {
+      activeFooter.style.display = "none";
+    }
+  });
+}
